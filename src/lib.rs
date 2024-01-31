5395966b276b04651fe8f8bf1c7b815a862a5a6b
sebasti810 <spusch@outlook.de>
2024-01-31 09:55:35 +0100

    feat: add filepath to filestore struct, add enum to choose

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,31 @@ pub trait KeyStore {
     fn get_signing_key(&self) -> Result<SigningKey, String>;
 }
 
+pub enum KeyStoreType {
+    KeyChain(KeyChain),
+    FileStore(FileStore),
+    // tbd
+}
+
+impl KeyStore for KeyStoreType {
+    fn add_signing_key(&self, signing_key: &SigningKey) -> Result<(), String> {
+        match self {
+            KeyStoreType::KeyChain(store) => store.add_signing_key(signing_key),
+            KeyStoreType::FileStore(store) => store.add_signing_key(signing_key),
+            // m,ore to come
+        }
+    }
+
+    fn get_signing_key(&self) -> Result<SigningKey, String> {
+        match self {
+            KeyStoreType::KeyChain(store) => store.get_signing_key(),
+            KeyStoreType::FileStore(store) => store.get_signing_key(),
+            // todo: more cases
+        }
+    }
+}
+
+
 // TODO: keychain is only for macos, should there be another abstraction to handle linux and windows as well
 pub struct KeyChain;
 impl KeyStore for KeyChain {
@@ -28,14 +53,27 @@ impl KeyStore for KeyChain {
     }
 }
 
-pub struct FileStore;
+pub struct FileStore {
+    file_path: String,
+}
+
+impl FileStore {
+    // using impl Into<String> to allow for &str and String here 
+    pub fn new(file_path: impl Into<String>) -> Self {
+        FileStore {
+            file_path: file_path.into(),
+        }
+    }
+}
+
+
 impl KeyStore for FileStore {
     // TODO: how to handle filepaths better / the right way?
     fn add_signing_key(&self, signing_key: &SigningKey) -> Result<(), String> {
-        encrypt_and_store_private_key(signing_key, "signing_key").map_err(|e| e.to_string())
+        encrypt_and_store_private_key(signing_key, &self.file_path).map_err(|e| e.to_string())
     }
 
     fn get_signing_key(&self) -> Result<SigningKey, String> {
-        load_and_decrypt_private_key("signing_key").map_err(|e| e.to_string())
+        load_and_decrypt_private_key(&self.file_path).map_err(|e| e.to_string())
     }
 }
