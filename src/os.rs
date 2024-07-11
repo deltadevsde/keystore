044d06f429400ff71544daac664d10f865851939
sebasti810 <spusch@outlook.de>
2024-07-11 15:04:03 +0200

    fix: imports

--- src/os.rs
+++ src/os.rs
@@ -4,8 +4,10 @@ use std::error::Error;
 #[cfg(feature = "macos")]
 use security_framework::os::macos::keychain::SecKeychain;
 
-#[cfg(target_os = "linux")]
-use keyring::{Entry, Keyring};
+#[cfg(feature = "linux")]
+use base64::{engine::general_purpose, Engine as _};
+#[cfg(feature = "linux")]
+use keyring::Entry;
 
 use crate::create_signing_key;
 
@@ -22,10 +24,10 @@ pub fn add_signing_key_to_keychain(
 #[cfg(feature = "linux")]
 pub fn add_signing_key_to_keychain(signing_key: &SigningKey) -> Result<(), Box<dyn Error>> {
     let signing_key_bytes = signing_key.to_bytes();
-    let signing_key_str = base64::encode(&signing_key_bytes);
+    let signing_key_str = general_purpose::STANDARD.encode(&signing_key_bytes);
 
-    let keyring = Keyring::new("deimos", "signing_key");
-    keyring.set_password(&signing_key_str)?;
+    let entry = Entry::new("deimos", "signing_key").unwrap();
+    entry.set_password(&signing_key_str)?;
 
     Ok(())
 }
@@ -51,18 +53,18 @@ pub fn get_signing_key_from_keychain() -> Result<SigningKey, Box<dyn Error>> {
 
 #[cfg(feature = "linux")]
 pub fn get_signing_key_from_keychain() -> Result<SigningKey, Box<dyn Error>> {
-    let keyring = Keyring::new("deimos", "signing_key");
+    let keyring = Entry::new("deimos", "signing_key")?;
 
     let signing_key_str = match keyring.get_password() {
         Ok(password) => password,
         Err(_) => {
             let signing_key = create_signing_key();
             add_signing_key_to_keychain(&signing_key)?;
-            base64::encode(signing_key.to_bytes())
+            general_purpose::STANDARD.encode(signing_key.to_bytes())
         }
     };
 
-    let signing_key_bytes = base64::decode(&signing_key_str)?;
+    let signing_key_bytes = general_purpose::STANDARD.decode(&signing_key_str)?;
     let mut signing_key_array = [0u8; 32];
     signing_key_array.copy_from_slice(&signing_key_bytes[..32]);
     Ok(SigningKey::from_bytes(&signing_key_array))
